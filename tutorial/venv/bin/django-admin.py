be916b6f39d693d4f125871fc9033aa7ff2c3f0c
Victor Olimpio <victorolimpio@Victors-MacBook-Pro.local>
2019-11-23 00:00:28 -0300

    first commit

---
+++ tutorial/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/victorolimpio/PycharmProjects/django-drf-tutorial/tutorial/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
